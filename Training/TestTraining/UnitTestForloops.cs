e49c07a040368bdcca9f0e2b30c519b8a8fa1da9
mpeder75 <mpeder75@gmail.com>
2026-03-08 21:59:43 +0100

    Traing project oprettet

---
+++ Training/TestTraining/UnitTestForloops.cs
@@ -0,0 +1,20 @@
+﻿using Training.forloops;
+
+namespace TestTraining
+{
+    public class UnitTestForloops
+    {
+        [Fact]
+        public void Test()
+        {
+            // Arrange
+            List<int> testList = new List<int>{1,2,3,4,5,6,7,8,9};
+
+            // Act
+            Forloop forloop = new Forloop();
+
+            // Assert
+            Assert.Equal(forloop.SumList(testList),45);
+        }
+    }
+}
---
+++ Training/Training/forloops/Forloop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training.forloops
+{
+    public class Forloop
+    {
+
+        public double SumList(List<int> myList)
+        {
+            double result = 0;
+
+            for(int i =0; i < myList.Count; i++)
+            {
+                result += myList[i];
+            }
+
+            return result;
+        }
+
+
+    }
+}
